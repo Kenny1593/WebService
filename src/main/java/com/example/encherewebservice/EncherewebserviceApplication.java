f4188414ccd1d80f9f15c8d1f4c17a49fe83794e
KennyRk <99263825+KennyRk@users.noreply.github.com>
2023-01-20 14:56:23 +0300

    webservice

---
+++ src/main/java/com/example/encherewebservice/EncherewebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.encherewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncherewebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncherewebserviceApplication.class, args);
+	}
+
+}
